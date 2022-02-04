ea6756916827aaa81f5411d23f1ba170cdc936b2
a_lyubinskiy <msanvold@gmail.com>
2022-02-04 17:35:40 +0300

    add allure open

---
+++ PyTestConcepts/allure_open.py
@@ -0,0 +1,14 @@
+import shutil
+import subprocess
+
+
+def get_allure_exec():
+    allures_exe = ['allure.cmd', 'allure.bat', 'allure']
+    for allure_exe in allures_exe:
+        if shutil.which(allure_exe):
+            return allure_exe
+    raise OSError(f'Expected allure executables {allures_exe} do not exist!')
+
+
+if __name__ == '__main__':
+    subprocess.check_call([get_allure_exec(), 'open'])
